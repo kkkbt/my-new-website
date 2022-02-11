f7a3d98200efd95b3dcecfb40f3a3dc8b83ee43e
Kenta Kubota <tokyo.tech1999soccer@gmail.com>
2022-02-11 19:03:14 +0900

    Complete title screens.

--- main.py
+++ main.py
@@ -1,49 +1,49 @@
 import os
 
-
 from dotenv import load_dotenv
 from flask import Flask, render_template
-
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+print(os.getenv("SECRET_KEY"))
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    contents = ["profile", "library", "biography", "investment", "contact"]
+    return render_template("index.html", contents=contents)
 
 
 @app.route("/application")
 def application():
-    return render_template("application.html")
+    return render_template("application.html", title='application')
+
 
 @app.route("/library")
 def library():
-    return render_template("library.html")
+    return render_template("library.html", title='library')
 
 
 @app.route("/biography")
 def biography():
-    return render_template("biography.html")
+    return render_template("biography.html", title='biography')
 
 
 @app.route("/profile")
 def profile():
-    return render_template("profile.html")
+    return render_template("profile.html", title='profile')
 
 
 @app.route("/investment")
 def investment():
-    return render_template("investment.html")
+    return render_template("investment.html", title='investment')
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", title='contact')
 
 
 if __name__ == '__main__':
-
     app.run(debug=True)
