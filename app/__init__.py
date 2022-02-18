a90a331e38cf1daf1d068e96004f542e856c3896
Kenta Kubota <tokyo.tech1999soccer@gmail.com>
2022-02-18 17:06:20 +0900

    to create database in postgresql.

--- app/__init__.py
+++ app/__init__.py
@@ -20,3 +20,4 @@ login_manager.login_message = u"Bonvolu ensaluti por uzi tiun paĝon."
 
 from app import view
 from app import database
+
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from app import app
 from app import db
-
+from app.database import UserDatabase, PortfolioDatabase, LibraryDatabase, BiographyDatabase, ProfileDatabase
 
 if __name__ == '__main__':
     db.create_all()
