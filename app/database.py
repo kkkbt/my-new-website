43ef90be7a9c4ae6ab1d9c55dda473486dda626d
Kenta Kubota <tokyo.tech1999soccer@gmail.com>
2022-02-19 21:53:50 +0900

    Update setting page and create secret page.

--- app/database.py
+++ app/database.py
@@ -5,55 +5,65 @@ from flask_login import UserMixin
 class UserDatabase(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     password = db.Column(db.String(1000))
-    name = db.Column(db.String(1000), unique=True)
+    name = db.Column(db.String(50), unique=True)
     columns = ["name", "password"]
+
+
+class Secret(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    kind = db.Column(db.String(50), nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    staff = db.Column(db.String(50), nullable=False)
+    examples = db.Column(db.String(1000), nullable=False)
+
+    columns = ["kind", "title", "staff", "examples"]
 
 
 class ProfileDatabase(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    kind = db.Column(db.String(250), nullable=False)
-    title = db.Column(db.String(250), nullable=False)
-    staff = db.Column(db.String(250), nullable=False)
-    examples = db.Column(db.String(250), nullable=True)
+    kind = db.Column(db.String(50), nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    staff = db.Column(db.String(50), nullable=False)
+    examples = db.Column(db.String(50), nullable=False)
 
     columns = ["kind", "title", "staff", "examples"]
 
 
 class PortfolioDatabase(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    kind = db.Column(db.String(250), nullable=False)
-    title = db.Column(db.String(250), unique=True, nullable=True)
-    date = db.Column(db.String(250), nullable=True)
-    link_title = db.Column(db.String(250), nullable=True)
-    link_url = db.Column(db.String(250), nullable=True)
-    description = db.Column(db.String(250), nullable=True)
-    img = db.Column(db.String(250), nullable=True)
+    kind = db.Column(db.String(50), nullable=False)
+    title = db.Column(db.String(50), unique=True, nullable=False)
+    date = db.Column(db.String(50), nullable=True)
+    link_title = db.Column(db.String(50), nullable=True)
+    link_url = db.Column(db.String(1000), nullable=True)
+    description = db.Column(db.String(1000), nullable=True)
+    img = db.Column(db.String(50), nullable=False)
 
     columns = ["kind", "title", "date", "link_title", "link_url", "description", "img"]
 
 
 class LibraryDatabase(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    kind = db.Column(db.String(250), nullable=False)
-    title = db.Column(db.String(250), nullable=False)
-    link_title = db.Column(db.String(250), nullable=True)
-    link_url = db.Column(db.String(250), nullable=True)
-    description = db.Column(db.String(250), nullable=True)
+    kind = db.Column(db.String(50), nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    link_title = db.Column(db.String(50), nullable=False)
+    link_url = db.Column(db.String(1000), nullable=False)
+    description = db.Column(db.String(1000), nullable=True)
 
     columns = ["kind", "title", "link_title", "link_url", "description"]
 
 
 class BiographyDatabase(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    kind = db.Column(db.String(250), nullable=False)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    date = db.Column(db.String(250), nullable=True)
-    place = db.Column(db.String(250), nullable=True)
-    member = db.Column(db.String(250), nullable=True)
-    link_title = db.Column(db.String(250), nullable=True)
-    link_url = db.Column(db.String(250), nullable=True)
-    description = db.Column(db.String(250), nullable=True)
-    img = db.Column(db.String(250), nullable=True)
+    kind = db.Column(db.String(50), nullable=False)
+    title = db.Column(db.String(50), unique=True, nullable=False)
+    date = db.Column(db.String(50), nullable=True)
+    place = db.Column(db.String(50), nullable=True)
+    member = db.Column(db.String(50), nullable=True)
+    link_title = db.Column(db.String(50), nullable=True)
+    link_url = db.Column(db.String(1000), nullable=True)
+    description = db.Column(db.String(1000), nullable=True)
+    img = db.Column(db.String(50), nullable=False)
 
     columns = ["kind", "title", "date", "place", "member", "link_title", "link_url", "description", "img"]
 
